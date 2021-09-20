eeaf1eb893dd40dea319a119502c0acc9e433339
Matthew Lese <matthewjlese@gmail.com>
2021-09-20 10:47:49 -0700

    dfs algorithm started

--- W3/polytreenode/skeleton/lib/00_tree_node.rb
+++ W3/polytreenode/skeleton/lib/00_tree_node.rb
@@ -1,27 +1,45 @@
 class PolyTreeNode
 
-    attr_reader :parent, :children, :value
+  attr_reader :parent, :children, :value
 
-    def initialize(value)
-        @value = value
-        @parent = nil
-        @children = []
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent=(parent_node) 
+    prev_parent = self.parent
+    @parent = parent_node
+    return nil if parent == nil
+    if prev_parent == nil
+      if !parent_node.children.include?(self)
+        parent_node.children << self   
+      end   
+    else #current node has a previous parent
+      prev_parent.children.delete(self)
+      parent_node.children << self   
     end
+  end
 
-    def parent=(parent_node) 
-        prev_parent = self.parent
-        @parent = parent_node
-        return nil if parent == nil
-        if prev_parent == nil
-            if !parent_node.children.include?(self)
-                parent_node.children << self   
-            end   
-        else #current node has a previous parent
-            prev_parent.children.delete(self)
-            parent_node.children << self   
-        end
+  def add_child(node)
+    node.parent = self if node.parent.nil?
+  end
+
+  def remove_child(node)
+    raise "Node is not a child" if node.parent == nil
+    node.parent = nil
+  end
+
+  def dfs(value)
+    return nil if self.nil?
+    stack = [self]
+    if self.value == value
+      return self
+    else
+      self.children.first.dfs(value)
     end
-
+  end
 
 
 end
